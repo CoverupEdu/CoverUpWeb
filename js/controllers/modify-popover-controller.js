eee61f49159612cc45a2a9494f04df44cd4eed42
schligg <hugoaaronson64@gmail.com>
2016-08-13 16:47:00 +0100

    Update modify-popover-controller.js

--- js/controllers/modify-popover-controller.js
+++ js/controllers/modify-popover-controller.js
@@ -1,19 +1,21 @@
 app.controller('modify-popover-controller', function($scope, $ionicPopover, $rootScope, $timeout) {
-	$scope.insLabel;
+	$scope.insLabel;	//currently entered label while in edit mode.
 
 	$rootScope.textFocus = function(){
-		if ($rootScope.labelEdit) {
+		if ($rootScope.canEditLabel) {
 			$timeout(function () {
 			document.getElementById('textEntry').focus();
 			if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 			  cordova.plugins.Keyboard.show(); //open keyboard manually
 			}
-		  }, 350);
+		  }, 500);
 	}};
+	
+	//'focus' text such that the textbox becomes selected (on phone, keyboard comes up etc.)
 	
 	$scope.$on('popover.hidden', function() {
 		$rootScope.editButton();
-		if ($rootScope.labelEdit) {$rootScope.labelEdit = false;}
+		if ($rootScope.canEditLabel) {$rootScope.canEditLabel = false;}
 		$timeout(function() {
 			if ($rootScope.popOpen) {
 				$rootScope.checkNull();
@@ -22,24 +24,31 @@ app.controller('modify-popover-controller', function($scope, $ionicPopover, $roo
 		})
 	});
 	
+	//save currently entered label, set edit mode to false for next label, and delete popover if label is null.
+	
 	$scope.editStyle = function() {
-		if ($rootScope.labelEdit) {
+		if ($rootScope.canEditLabel) {
 			return "button energized button-icon icon ion-android-done";
 		} else {
 			return "button energized button-icon icon ion-edit";
 		}
 	}
 	
+	//define how edit button looks whether in edit/view mode for label.
 	
 	$rootScope.editButton = function() {
 		$rootScope.curLabel = $scope.insLabel;
-		if (!$rootScope.labelEdit) {
+		if (!$rootScope.canEditLabel) {
 			$scope.insLabel = $scope.labels[$scope.curIndex].label;
 		}
 		else {$scope.labels[$scope.curIndex].label = $rootScope.curLabel;}
-		$rootScope.labelEdit = !$rootScope.labelEdit;
+		$rootScope.canEditLabel = !$rootScope.canEditLabel;
 		$rootScope.textFocus();
 	};
+	
+	/*actions when 'edit' button is clicked. currently entered label is saved as actual label, 
+	and if going from viewing to editing, the currently entered label is defined as the actual label.
+	*/
 	
 	$rootScope.insReset = function() {
 		$scope.insLabel = "";
